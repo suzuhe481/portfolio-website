a010ab2b77f8284b34fecc2bd9580c33af871cd8
Hector <baseballhector@gmail.com>
2022-10-17 14:14:07 -0400

    Creating route for NASA project

--- start.py
+++ start.py
@@ -9,3 +9,7 @@ def index():
 @app.route("/about/")
 def about():
     return render_template('about.html', title = "About me")
+
+@app.route("/nasa-picture-of-the-day/", methods=['POST', 'GET']) 
+def nasa_api_project():
+    return render_template('projects/nasa-picture-of-the-day.html', title = "NASA Astronomy Picture of the Day")
