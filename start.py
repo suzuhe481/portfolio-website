24445da64944de697ccaed6e69caae66bb7e103b
Hector <baseballhector@gmail.com>
2022-10-31 21:20:38 -0400

    Changes how it accepts incoming form data

--- start.py
+++ start.py
@@ -15,25 +15,34 @@ def about():
 
 @app.route("/nasa-picture-of-the-day/", methods=['POST', 'GET']) 
 def nasa_api_project():
-    # Retrieving today's Nasa's Astronomy Picture of the Day.
-    if request.method == "POST" and "getToday" in request.form:
-        returned_APOD_Data = nasaModule.getNasaAPOD(config.nasa_API_key)   # Gets the response object.
-        returned_APOD_JSON = returned_APOD_Data.json()              # Takes a response and returns a promise.
+    # Recieving data from the form.
+    if request.method == "POST" and "nasa-form" in request.form:
+        formData = request.form.to_dict()
 
-    # Retrieving a random Nasa's Astronomy Picture of the Day.
-    elif request.method == "POST" and "getRandom" in request.form:
-        returned_APOD_Data = nasaModule.getRandomNasaAPOD(config.nasa_API_key)
-        returned_APOD_JSON = returned_APOD_Data.json()
+        # Retrieving today's Nasa's Astronomy Picture of the Day.
+        if formData.get("nasa-form") == "getToday":
+            print("Getting today's NASA image")
 
-    # Retrieving a user specified Nasa's Astronomy Picture of the Day.
-    elif request.method == "POST" and "getCustom" in request.form:
-        pickedDate = request.form.get("pickedDate")
+            returned_APOD_Data = nasaModule.getNasaAPOD(config.nasa_API_key)        # Gets the response object.
+            returned_APOD_JSON = returned_APOD_Data.json()                          # Takes a response and returns a promise.
 
-        returned_APOD_Data = nasaModule.getCustomNasaAPOD(config.nasa_API_key, pickedDate)   # Gets the response object.
-        returned_APOD_JSON = returned_APOD_Data.json()              # Takes a response and returns a promise.
+        # Retrieving a random Nasa's Astronomy Picture of the Day.
+        elif formData.get("nasa-form") == "getRandom":
+            print("Getting a random Nasa image")
 
+            returned_APOD_Data = nasaModule.getRandomNasaAPOD(config.nasa_API_key)  # Gets the response object.
+            returned_APOD_JSON = returned_APOD_Data.json()                          # Takes a response and returns a promise.
+        # Retrieving a user specified Nasa's Astronomy Picture of the Day.
+        elif formData.get("nasa-form") == "getCustom":
+            print("Getting a custom dated NASA image")
+            pickedDate = formData.get("pickedDate")
+
+            returned_APOD_Data = nasaModule.getCustomNasaAPOD(config.nasa_API_key, pickedDate)      # Gets the response object.
+            returned_APOD_JSON = returned_APOD_Data.json()                                          # Takes a response and returns a promise.
+
+    # Renders default NASA project page.
     else:
         return render_template('projects/nasa-picture-of-the-day.html', title = "NASA Astronomy Picture of the Day")
 
-    # Returning template with data.
+    # Renders NASA project page with data.
     return render_template('projects/nasa-picture-of-the-day.html', title = "NASA Astronomy Picture of the Day", nasa_Data=returned_APOD_JSON)
